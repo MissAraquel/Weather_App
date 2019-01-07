2a2c3f18ca503502d1f3400a7b8b96b71421d630
Miss Araquel <missaraquel@gmail.com>
2019-01-06 20:07:52 -0800

    Updates

---
+++ precache-manifest.6671ccf56de0a720b9f2d3b7bcb88a93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "6a3821e9283cda701921",
+    "url": "/weather_app/static/css/main.5322bc19.chunk.css"
+  },
+  {
+    "revision": "ea6289f8679b32a74a2f95dc1662d2b7",
+    "url": "/weather_app/static/media/background.ea6289f8.jpg"
+  },
+  {
+    "revision": "934782d2a1fa6a822567",
+    "url": "/weather_app/static/js/1.934782d2.chunk.js"
+  },
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "6a3821e9283cda701921",
+    "url": "/weather_app/static/js/main.6a3821e9.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather_app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "934782d2a1fa6a822567",
+    "url": "/weather_app/static/css/1.bcff7603.chunk.css"
+  },
+  {
+    "revision": "3cd294c10277e8f8677ed930b05cacc3",
+    "url": "/weather_app/index.html"
+  }
+];
